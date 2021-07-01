e1905ac4d2078515d5aa50351641a68235cf75ce
ANKIT KENE <keneankit01@gmail.com>
2021-07-01 16:00:30 +0530

    Switch  To My Ota repo url for unofficial

--- src/org/pixelexperience/ota/misc/Constants.java
+++ src/org/pixelexperience/ota/misc/Constants.java
@@ -31,8 +31,8 @@ public final class Constants {
     static final String PROP_DEVICE = "org.pixelexperience.device";
     static final String PROP_BUILD_VERSION = "org.pixelexperience.version";
     static final String OTA_URL = "https://download.pixelexperience.org/ota_v5/%s/%s";
-    static final String OTA_CI_URL = "https://download.pixelexperience.org/ota_ci/%s/%s";
-    static final String MAINTAINER_URL = "https://download.pixelexperience.org/team/%s";
+    static final String OTA_CI_URL = "https://raw.githubusercontent.com/Ankits-Updates/OTA/main/builds/PixelExperience/%s.json";
+    static final String MAINTAINER_URL = "https://www.keneankit.codes/";
     static final String DOWNLOAD_WEBPAGE_URL = "https://download.pixelexperience.org/changelog/%s/%s";
     static final String EXPORT_PATH = "PixelExperience-Updates/";
 }
